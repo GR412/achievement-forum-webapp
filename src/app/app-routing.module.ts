614291a4be0d16a712acd8711454022cc9581eb9
Grant <gr412@hotmail.co.uk>
2020-02-24 11:43:58 +0000

    added a page not found component to handle invalid urls. Also made the default page redirects to the home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AdminComponent} from './admin/admin.component';
 import {HomeComponent} from './home/home.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const routes: Routes  = [{path: 'login', component: LoginComponent},
+const routes: Routes  = [
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'login', component: LoginComponent},
   {path: 'admin', component: AdminComponent},
-  {path: '', component: HomeComponent}];
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: PageNotFoundComponent}]; // must always be at the bottom
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -14,4 +18,4 @@ const routes: Routes  = [{path: 'login', component: LoginComponent},
 })
 
 export  class AppRoutingModule {}
-export const routingComponents = [HomeComponent, AdminComponent, LoginComponent];
+export const routingComponents = [HomeComponent, AdminComponent, LoginComponent, PageNotFoundComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 
 import { AuthenticateService } from './authenticate.service';
 import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
