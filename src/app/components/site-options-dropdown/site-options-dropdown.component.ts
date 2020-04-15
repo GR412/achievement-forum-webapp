944a77d0bddbe35e97e0d5ce2bebd89cf25d24ed
Grant <gr412@hotmail.co.uk>
2020-04-15 14:17:40 +0100

    changed all css files to use the scss file extension

--- src/app/components/site-options-dropdown/site-options-dropdown.component.ts
+++ src/app/components/site-options-dropdown/site-options-dropdown.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-site-options-dropdown',
   templateUrl: './site-options-dropdown.component.html',
-  styleUrls: ['./site-options-dropdown.component.css']
+  styleUrls: ['./site-options-dropdown.component.scss']
 })
 export class SiteOptionsDropdownComponent implements OnInit {
 
