09f7ea3adc608078dbca14b74a2ae6bb68dc7e7d
Grant <gr412@hotmail.co.uk>
2020-04-23 11:14:08 +0100

    Implemented the site options drop down

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public siteOptionsToggle = false;
+  public toggleCount = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleSiteOptions() {
+
+   /* if (this.toggleCount === 1) {*/
+    this.siteOptionsToggle = !this.siteOptionsToggle;
+   /* } else if (this.toggleCount === 2) {
+    this.siteOptionsToggle = false;*/
+ // }
+  }
 }
